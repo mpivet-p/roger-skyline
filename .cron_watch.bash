7a672c33e3046686b7de258b14d8b0f5d2fc1acf
mpivet-p <45142887+mpivet-p@users.noreply.github.com>
2019-03-21 15:58:43 +0100

    Update .cron_watch.bash

--- .cron_watch.bash
+++ .cron_watch.bash
@@ -1,9 +1,8 @@
-#!bin/bash
+#!/bin/bash
 
 ROOT_MAIL="root@localhost"
 REF_SUM="8f111d100ea459f68d333d63a8ef2205  /etc/crontab"
 MD5SUM=$(sudo md5sum /etc/crontab)
-
 
 if [ "${REF_SUM}" != "${MD5SUM}" ];
 	then
