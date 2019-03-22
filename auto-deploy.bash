1f4a581850ad23100e9a86c97a3fa476402977d0
mpivet-p <45142887+mpivet-p@users.noreply.github.com>
2019-03-22 19:57:31 +0100

    Update auto-deploy.bash

--- auto-deploy.bash
+++ auto-deploy.bash
@@ -36,13 +36,9 @@ then
 	read -p "Please choose a port for SSH Service : " PORT;
 	sed -i "13s/.*/Port $PORT/g" /home/$NAME/.server_conf/sshd_config
 	cp /home/$NAME/.server_conf/sshd_config /etc/ssh/sshd_config
-	#cp /home/$NAME/.server_conf/firewall /etc/init.d/firewall
 	cp /home/$NAME/.server_conf/.update.bash /home/$NAME/.update.bash
 	cp /home/$NAME/.server_conf/.cron_watch.bash /home/$NAME/.cron_watch.bash
 	cp /home/$NAME/.server_conf/interfaces /etc/network/interfaces
-	#chmod +x /etc/init.d/firewall
-	#/etc/init.d/firewall
-	#update-rc.d defaults firewall
 	echo "0 4 * * sun root bash /home/$NAME/.update.bash >> /var/log/update_script.log" >> /etc/crontab
 	echo "@reboot root bash /home/$NAME/.update.bash >> /var/log/update_script.log" >> /etc/crontab
 	echo "0 0 * * * root bash /home/$NAME/.cron_watch.bash" >> /etc/crontab
@@ -53,7 +49,8 @@ then
 	read -p "Please Choose an IP Address : " ADDRESS
 	echo -e "auto enp0s3\niface enp0s3 inet static\n\taddress $ADDRESS\n\tnetmask 255.255.255.252\n\tgateway $GATEWAY" >> /etc/network/interfaces
 	REF_SUM=$(md5sum /etc/crontab)
-	sed -i '4s|.*|REF_SUM="AAA"|' /home/$NAME/.server_conf/.cron_watch.bash
-	sed -i "4s|AAA|$REF_SUM|" /home/$NAME/.server_conf/.cron_watch.bash
+	sed -i "" '4s|root:.*|root: root' /etc/aliases
+	sed -i "" '4s|.*|REF_SUM="AAA"|' /home/$NAME/.server_conf/.cron_watch.bash
+	sed -i "" "4s|AAA|$REF_SUM|" /home/$NAME/.server_conf/.cron_watch.bash
 fi
 apt-get install apache2
