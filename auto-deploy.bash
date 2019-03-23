c8353ff74ff49312381efd08fef151592242fc25
Maxime PIVET-PIERRE <mpivet-p@e3r10p18.42.fr>
2019-03-23 23:29:06 +0100

    nedd firewall

--- auto-deploy.bash
+++ auto-deploy.bash
@@ -20,37 +20,59 @@ fi
 if [ $WEB == "Y" ] || [ $WEB == "y" ]
 then
 	# PACKAGE DOWNLOADING
-	apt-get install sudo
-	apt-get install vim
-	apt-get install git
-	apt-get install openssh-server
-	apt-get install net-tools
-	apt-get install iptables
-	apt-get install mailutils
-	apt-get install sendmail-bin
-	apt-get install sendmail
-	apt-get install fail2ban
+	apt-get install sudo -y
+	apt-get install vim -y
+	apt-get install git -y
+	apt-get install openssh-server -y
+	apt-get install net-tools -y
+	apt-get install iptables -y
+	apt-get install mailutils -y
+	apt-get install sendmail-bin -y
+	apt-get install sendmail -y
+	apt-get install fail2ban -y
 
 	# CONFIG
 	git clone "https://github.com/mpivet-p/server_conf.git" /home/$NAME/.server_conf;
+
+	# SSH SETUP
 	read -p "Please choose a port for SSH Service : " PORT;
 	sed -i "13s/.*/Port $PORT/g" /home/$NAME/.server_conf/sshd_config
 	cp /home/$NAME/.server_conf/sshd_config /etc/ssh/sshd_config
-	cp /home/$NAME/.server_conf/.update.bash /home/$NAME/.update.bash
-	cp /home/$NAME/.server_conf/.cron_watch.bash /home/$NAME/.cron_watch.bash
-	cp /home/$NAME/.server_conf/interfaces /etc/network/interfaces
-	echo "0 4 * * sun root bash /home/$NAME/.update.bash >> /var/log/update_script.log" >> /etc/crontab
-	echo "@reboot root bash /home/$NAME/.update.bash >> /var/log/update_script.log" >> /etc/crontab
-	echo "0 0 * * * root bash /home/$NAME/.cron_watch.bash" >> /etc/crontab
-	echo -e "$NAME\tALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 	mkdir /home/$NAME/.ssh
 	cp /home/$NAME/.server_conf/authorized_keys /home/$NAME/.ssh/authorized_keys
+	service ssh restart
+	service sshd restart
+
+	# CRON SETUP
+	cp /home/$NAME/.server_conf/.update.bash /home/$NAME/.update.bash
+	cp /home/$NAME/.server_conf/.cron_watch.bash /home/$NAME/.cron_watch.bash
+	echo -e "00 4\t* * sun\troot\tbash /home/$NAME/.update.bash >> /var/log/update_script.log" >> /etc/crontab
+	echo -e "@reboot\troot\tbash /home/$NAME/.update.bash >> /var/log/update_script.log" >> /etc/crontab
+	echo -e "00 0\t* * *\troot\tbash /home/$NAME/.cron_watch.bash" >> /etc/crontab
+	REF_SUM=$(md5sum /etc/crontab)
+	sed -i '4s|.*|REF_SUM="AAA"|' /home/$NAME/.server_conf/.cron_watch.bash
+	sed -i "4s|AAA|$REF_SUM|" /home/$NAME/.server_conf/.cron_watch.bash
+	service cron restart
+
+	# SUDO SETUP
+	echo -e "$NAME\tALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+	service sudo restart
+
+	# NETWORK INTERFACES SETUP
+	cp /home/$NAME/.server_conf/interfaces /etc/network/interfaces
 	GATEWAY=$(route -n | sed -n '3p' | awk '{print $2}')
 	read -p "Please Choose an IP Address : " ADDRESS
 	echo -e "auto enp0s3\niface enp0s3 inet static\n\taddress $ADDRESS\n\tnetmask 255.255.255.252\n\tgateway $GATEWAY" >> /etc/network/interfaces
-	REF_SUM=$(md5sum /etc/crontab)
-	sed -i "" '4s|root:.*|root: root' /etc/aliases
-	sed -i "" '4s|.*|REF_SUM="AAA"|' /home/$NAME/.server_conf/.cron_watch.bash
-	sed -i "" "4s|AAA|$REF_SUM|" /home/$NAME/.server_conf/.cron_watch.bash
+	service networking restart
+
+	# MAIL PATCH
+	sed -i '4s|root:.*|root: root' /etc/aliases
 fi
-apt-get install apache2
+
+# BEGINNING WEBSITE DEPLOY
+apt-get install apache2 -y
+git clone https://github.com/mpivet-p/RS_WEB.git /var/www/html/www.myroger.fr
+rm -rf /etc/apache2/sites-available/*
+mv /var/www/html/www.myroger.fr/00-www.myroger.fr.conf /etc/apache2/site-available/00-www.myroger.fr.conf
+a2ensite 00-www.myroger.com
+service apache2 restart
